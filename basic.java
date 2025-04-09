e678914e697336434e73d569823d4fa8b8a4f2d2
poornima-S3456 <prathasingh8536885@gmail.com>
2025-04-09 11:00:03 +0530

    msg

--- basic.java
+++ basic.java
@@ -1,11 +1,22 @@
 public class basic {
     public static void main( String[] args){
         int a= 20;
-        int b= 30;
-        a= a+b;
-        b= a-b;
-        a= a-b;
-        System.out.println( "a="+a+  " b="+b);
+        int b= 50;
+         int c=40;
+        // a= a+b;
+        // b= a-b;
+        // a= a-b;
+        // System.out.println( "a="+a+  " b="+b);
+        if(a>b && a>c){
+            System.out.println("a is greatest");
+        }
+        else if(b>c){
+            System.out.println("b is greatest");
+        }
+        else{
+            System.out.println("c is greatest");
+        }
+
 
     }
     
