59c1c65e9de586bfcf5a5d7451c959152154c2c3
poornima-S3456 <prathasingh8536885@gmail.com>
2025-04-10 20:41:36 +0530

    msg

--- basic.java
+++ basic.java
@@ -1,23 +1,43 @@
 public class basic {
     public static void main( String[] args){
-        int a= 20;
-        int b= 50;
-         int c=40;
+        // int a= 20;
+        // int b= 50;
+        //  int c=40;
         // a= a+b;
         // b= a-b;
         // a= a-b;
         // System.out.println( "a="+a+  " b="+b);
-        if(a>b && a>c){
-            System.out.println("a is greatest");
-        }
-        else if(b>c){
-            System.out.println("b is greatest");
-        }
-        else{
-            System.out.println("c is greatest");
-        }
+    // if(a>b&& a>c){
+    //         System.out.println("a is greatest");
+    //     }
+    //     else if(b>c){
+    //         System.out.println("b is greatest");
+    //     }
+    //     else{
+    //         System.out.println("c is greatest");
+    //     }
+    // int year = 2027;
+    // if( year % 4==0 &&(year %100!= 0 || year % 400 == 0)){
+    //     System.out.println( "Leap year");
+    // }
+    // else{
+    //     System.out.println("not a leap year");
+    // }
+//     int num = 7;
+//     boolean isPrime = true;
+
+//     for(int i=2; i<= num/2; i++){
+//         if ( num%i == 0){
+//         isPrime = true;
+//         break;
+
+
+//     }
+// }
+// System.out.println( isPrime);
 
 
     }
     
 }
+   
