15ac84d0f48535215576084448faeaa5e9e57cd1
poornima-S3456 <prathasingh8536885@gmail.com>
2025-04-15 14:06:18 +0530

    mag

---
+++ different.java
@@ -0,0 +1,71 @@
+public class different {
+    public static void main( String[] args){
+        for( int i=1;i<=5;i++){
+            for( int j=1;j<=5-i;j++){
+                System.out.print( " ");
+            }
+            for( int j=1;j<=2*i-1;j++){
+            System.out.print( "*") ;
+        }
+        System.out.println( );
+    }
+   for( int i=5;i>=1;i--){
+    for( int j=1;j<=5-i;j++){
+        System.out.print( " ");
+    }
+    for( int j=1;j<=2*i-1;j++){
+    System.out.print( "*") ;
+}
+System.out.println( );
+}
+System.out.println();
+
+
+
+
+
+for( int p=5;p>=1;p--){
+    for( int q=1;q<=5-p;q++){
+        System.out.print(" " );
+    }
+    for( int r=1;r<=2*p-1;r++){
+        System.out.print( "*");
+    }
+    System.out.println();
+}
+System.out.println( );
+
+
+
+
+for( int i=1;i<=5;i++ ){
+    for( int j=1;j<=5;j++){
+        System.out.print( " *");
+    }
+    System.out.println( );
+}
+System.out.println();
+
+
+for( int i=1;i<=7;i++){
+    for( int j=1;j<=i;j++){
+        System.out.print( "*");
+    }
+    System.out.println();
+}
+
+
+for( int i=1; i<=5;i++){
+    char ch = 'A';
+    for( int j=1;j<=i;j++){
+        System.out.print( ch + " ");
+        ch++;
+    }
+    System.out.println();
+   
+}
+
+   } 
+    
+}
+
